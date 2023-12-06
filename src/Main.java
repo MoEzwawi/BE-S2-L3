29bebb8499f2bed46d6e62e427f62a57f24d22ca
MoEzwawi <mezwawi96@gmail.com>
2023-12-06 17:05:27 +0100

    finishes almost everything

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import entities.Customer;
+import entities.Order;
 import entities.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +11,50 @@ import java.util.Random;
 public class Main {
         private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        List<Product> productsList= new ArrayList<>();
+        // ******************** ESERCIZIO 1 **************************
+        List<Product> booksList= new ArrayList<>();
         for(int i=0;i<50;i++){
             Random rndm = new Random();
             int n = i + 1;
             Product newBook = new Product("Book "+n,"Books", rndm.nextDouble(100,1000));
-            productsList.add(newBook);
-            logger.info(newBook.toString());
+            booksList.add(newBook);
+            // logger.info(newBook.toString());
         }
+        // ******************** ESERCIZIO 2 **************************
+        List<Product> infancyItemsList= new ArrayList<>();
+        for(int i=0;i<50;i++){
+            Random rndm = new Random();
+            int n = i + 1;
+            Product infancyItem = new Product("Diaper "+n,"Baby", rndm.nextDouble(4,300));
+            infancyItemsList.add(infancyItem);
+        }
+        List<Order> ordersList = new ArrayList<>();
+        for(int i=0;i<20;i++) {
+            Random rndm = new Random();
+            List<Product> l = infancyItemsList.stream().filter(p -> p.getPrice()< rndm.nextDouble(40)).toList();
+            Customer c = new Customer("Customer n"+i,rndm.nextInt(1,6));
+            Order o = new Order(l,c);
+            ordersList.add(o);
+        }
+        //logger.info("List of baby products orders:");
+        //ordersList.forEach(order -> logger.info(order.toString()));
+
+        // ********************** ESERCIZIO 3 *********************************
+
+        List<Product> boysItemsList= new ArrayList<>();
+        for(int i=0;i<40;i++){
+            Random rndm = new Random();
+            int n = i + 1;
+            Product boysItem = new Product("Clothing"+n,"Boys", rndm.nextDouble(4,300));
+            boysItemsList.add(boysItem);
+        }
+        List<Product> salesBoysItemsList = boysItemsList.stream().map(i->{
+            Product newItem = new Product(i.getName(),i.getCategory(), i.getPrice()*0.9);
+            return newItem;
+        }).toList();
+        // boysItemsList.forEach(i->i.setPrice(i.getPrice()*0.9));
+
+        // ********************** ESERCIZIO 4 *********************************
+        List<Order> tier2Orders = ordersList.stream().filter(o->o.getCustomer().getTier()==2).toList();
     }
 }
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -57,4 +57,12 @@ public class Order {
     public void setProducts(List<Product> products) {
         this.products = products;
     }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "products=" + products +
+                ", customer=" + customer +
+                '}';
+    }
 }
